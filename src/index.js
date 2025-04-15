1e0ce9ba1a9c363024007fbc7618f4cd07d119a9
Kuldeep Kumar <upsainian3330@gmail.com>
2025-04-15 19:31:29 +0530

    db connection setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("hello world");
+import dotenv from "dotenv";
+import { connectDB } from "./db/index.js";
+import express from 'express'
+
+dotenv.config({
+    path:'./env'
+})
+const app=express()
+
+connectDB()
+
+
